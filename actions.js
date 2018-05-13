7328fececc6456a8cee59948d803f2efc098fbb5
Tomasz Paterka <tomekpaterka@gmail.com>
2018-05-13 12:15:35 +0200

    add exports to actions

--- actions.js
+++ actions.js
@@ -34,55 +34,56 @@ export const ADD_COMMENT = 'ADD_COMMENT',
 	thumbDown: 2,
 	id: 20
 }
-function addComment(text) {
+
+export function addComment(text) {
 	return {
 		type: ADD_COMMENT,
 		text,
 		id: uuid.v4()
 	}
 }
-const boundAddComment = text => dispatch(addComment(text));
+export const boundAddComment = text => dispatch(addComment(text));
 boundAddComment('new comment');
 
 
-function removeComment(id) {
+export function removeComment(id) {
 	return {
 		type: REMOVE_COMMENT,
 		id: id
 	}
 }
-const boundRemoveComment = id => dispatch(removeComment(id));
+export const boundRemoveComment = id => dispatch(removeComment(id));
 boundRemoveComment(id);
 
 
-function editComment(id, text) {
+export function editComment(id, text) {
 	return {
 		type: EDIT_COMMENT,
 		text,
 		id: id
 	}
 }
-const boundEditComment = (id, text) => dispatch(editComment(id, text));
+export const boundEditComment = (id, text) => dispatch(editComment(id, text));
 boundEditComment(id, 'edited comment');
 
 
-function thumbUpComment(id, thumbUp) {
+export function thumbUpComment(id, thumbUp) {
 	return {
 		type: THUMB_UP_COMMENT,
 		thumbUp: thumbUp++,
 		id: id
 	}
 }
-const boundThumbUp = (id, thumbUp) => dispatch(thumbUpComment(thumbUp, id));
+export const boundThumbUp = (id, thumbUp) => dispatch(thumbUpComment(thumbUp, id));
 boundThumbUp(id, thumbUp);
 
 
-function thumbDownComment(id, thumbDown) {
+export function thumbDownComment(id, thumbDown) {
 	return {
 		type: THUMB_DOWN_COMMENT,
 		thumbDown: thumbDown++,
 		id: id
 	}
 }
-const boundThumbDown = (id, thumbDown) => dispatch(thumbDownComment(thumbDown, id));
+export const boundThumbDown = (id, thumbDown) => dispatch(thumbDownComment(thumbDown, id));
 boundThumbUp(id, thumbDown);
